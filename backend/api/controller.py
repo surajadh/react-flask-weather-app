4afa826318561690059b484ac0fad1df7acf0da7
Suraj Adhikari <surajadh@gmail.com>
2022-10-12 23:02:42 -0500

    Formatting plus README changes

--- backend/api/controller.py
+++ backend/api/controller.py
@@ -4,15 +4,16 @@ from .service_layer.service_impl import ServiceImpl
 
 app = Flask(__name__)
 
-@app.route('/forecast')
+
+@app.route("/forecast")
 def get_current_weather():
-    zip = request.args.get('zip')
+    zip = request.args.get("zip")
     if not zip:
-        return 'zip needs to be provided', 400
+        return "zip needs to be provided", 400
 
     for c in zip:
-        if c not in '0123456789':
-            return 'not valid zip', 400
-            
-    service : Service = ServiceImpl()
+        if c not in "0123456789":
+            return "not valid zip", 400
+
+    service: Service = ServiceImpl()
     return service.get_min_max(zip)
